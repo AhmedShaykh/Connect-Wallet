3e68925094bf2ef2655b3e03afbaac9b0224e5c5
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-03-27 00:07:28 +0500

    Final Commit

--- Metamask/next.config.mjs
+++ Metamask/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_YWJsZS1tYXN0b2Rvbi0yMy5jbGVyay5hY2NvdW50cy5kZXYk",
+        CLERK_SECRET_KEY: "sk_test_cHrhr4nUoEyMl8WVQS1LbKS68DOw8LPZIUJJl9wxSz"
+    }
+};
 
 export default nextConfig;
