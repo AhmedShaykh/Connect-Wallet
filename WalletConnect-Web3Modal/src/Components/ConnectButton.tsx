84266edcdc195e4136a0d740e16342f543ebcf87
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-04-18 19:37:50 +0500

    Final Commit

--- WalletConnect-Web3Modal/src/Components/ConnectButton.tsx
+++ WalletConnect-Web3Modal/src/Components/ConnectButton.tsx
@@ -1,7 +1,43 @@
+"use client";
+import { formatAddress } from "@/lib/utils";
+import { useWalletInfo, useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount, useDisconnect } from "wagmi";
+
 const ConnectButton = () => {
+
+    const { address, isDisconnected } = useAccount();
+
+    const { disconnect } = useDisconnect();
+
+    const { open } = useWeb3Modal();
+
+    const { walletInfo } = useWalletInfo();
+
+    if (isDisconnected) {
+        return (
+            <div className="grid place-items-center h-screen">
+                <button
+                    className="bg-blue-700 py-3 px-6 rounded-full text-lg font-bold"
+                    onClick={() => open()}
+                >
+                    Connect Wallet
+                </button>
+            </div>
+        )
+    }
+
+    console.log(walletInfo?.icon, walletInfo?.name);
+
     return (
         <div className="grid place-items-center h-screen">
             <w3m-button />
+
+            <button
+                className="bg-zinc-800 py-3 px-5 rounded-full text-md font-semibold"
+                onClick={() => open({ view: "Account" }) || disconnect()}
+            >
+                {formatAddress(address)}
+            </button>
         </div>
     )
 };
